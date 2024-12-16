bac89c797c0bd4623a74775dee27f96561df2758
mini1115 <101314429+mini1115@users.noreply.github.com>
2024-12-16 15:55:35 +0900

    [level 3] Title: 자물쇠와 열쇠, Time: 29.80 ms, Memory: 89.3 MB -BaekjoonHub

--- 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.java
+++ 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.java
@@ -1,74 +1,57 @@
-import java.util.*;
 class Solution {
+    static int N;
     public boolean solution(int[][] key, int[][] lock) {
         boolean answer = false;
-        // 확장한 Lock을 만든다.
-        int N = lock.length;
-        int M = key.length;
-        
-        int newLockLength = N*3;
-        int[][] new_Lock = new int[newLockLength][newLockLength];
-        
-        for(int i=N;i<N*2;i++){
-            for(int j=N;j<N*2;j++){
-                new_Lock[i][j] = lock[i-N][j-N];
+        N = key.length;
+        int M = lock.length;
+        //새로운 lock만들기 약 3배 크기
+        int[][] new_lock = new int[M*3][M*3];
+        for(int i=0;i<M;i++){
+            for(int j=0;j<M;j++){
+                new_lock[i+M][j+M]=lock[i][j];
             }
         }
         
-//         for(int i=0;i<newLockLength;i++){
-//             for(int j=0;j<newLockLength;j++){
-//                 System.out.print(new_Lock[i][j]);
-//             }
-//             System.out.println();
-//         }
-        // 4방향 회전
-        for(int r=0;r<4;r++){
-            key = rotate(key);
-            
-            for(int x=0;x<N*2;x++){
-                for(int y=0;y<N*2;y++){
-                    for(int i=0;i<M;i++){
-                        for(int j=0;j<M;j++){
-                            new_Lock[x+i][y+j] += key[i][j];
+        for(int rotation=0;rotation<4;rotation++){
+            key=rotate(key);
+            for(int x=0;x<M*2;x++){
+                for(int y=0;y<M*2;y++){
+                    for(int i=0;i<N;i++){
+                        for(int j=0;j<N;j++){
+                            new_lock[x+i][y+j] +=key[i][j];
                         }
                     }
-                    if(check(new_Lock)){
+                    if(check(new_lock)){
                         return true;
                     }
                     else{
-                        for(int i=0;i<M;i++){
-                            for(int j=0;j<M;j++){
-                            new_Lock[x+i][y+j] -= key[i][j];
+                        for(int i=0;i<N;i++){
+                            for(int j=0;j<N;j++){
+                                new_lock[x+i][y+j] -= key[i][j];
                             }
                         }
                     }
                 }
             }
         }
-        
-        
-        
         return answer;
     }
-    
-    public boolean check(int[][] lock){
-        int len = lock.length /3;
-        for(int i=len;i<len*2;i++){
-            for(int j=len;j<len*2;j++){
-                if(lock[i][j]!=1){
+    static boolean check(int[][] new_lock){
+        int ll = new_lock.length/3;
+        for(int i=ll;i<ll*2;i++){
+            for(int j=ll;j<ll*2;j++){
+                if(new_lock[i][j]!=1){
                     return false;
                 }
             }
         }
         return true;
     }
-    
-    public int[][] rotate(int[][] key){
-        int len = key.length;
-        int[][] new_key = new int[len][len];
-        for(int i=0;i<len;i++){
-            for(int j=0;j<len;j++){
-                new_key[j][len-i-1]= key[i][j];
+    static int[][] rotate(int[][] key){
+        int[][] new_key = new int[N][N];
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+                new_key[j][N-i-1] = key[i][j];
             }
         }
         return new_key;
