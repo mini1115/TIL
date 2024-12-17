ee1482049468974deb6f6ec710626f262ec30579
mini1115 <101314429+mini1115@users.noreply.github.com>
2024-12-17 17:44:22 +0900

    [Silver III] Title: 소수 구하기, Time: 652 ms, Memory: 31800 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -10,13 +10,15 @@ public class Main {
         int startIndex = map[0];
         int endIndex = map[1];
 
+        boolean[] judge = isPrime2(endIndex);
+        
         for(int i=startIndex;i<=endIndex;i++){
-
-            if(isPrime(i)){
+            if(!judge[i]){
                 System.out.println(i);
             }
         }
-  }
+        
+    }
   static boolean isPrime(int x){
       if(x<2){
           return false;
@@ -29,4 +31,20 @@ public class Main {
       }
       return true;
   }
+//   에라토스테네스의 체
+    static boolean[] isPrime2(int x){
+        boolean[] judge = new boolean[x+1];
+        judge[1] = true;
+        for(int i=2;i<=Math.sqrt(x);i++){
+            int j=2;
+            if(!judge[i]){
+                while((i*j)<=x){
+                    judge[i*j] = true;
+                    j++;
+                }
+            }
+        }
+        return judge;
+        
+    }
 }
