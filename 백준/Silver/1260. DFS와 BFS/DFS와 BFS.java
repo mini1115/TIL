b354b6071c3e115a5e0a8a2ded0c34aac0ef0733
mini1115 <101314429+mini1115@users.noreply.github.com>
2024-12-24 13:33:42 +0900

    [Silver II] Title: DFS와 BFS, Time: 300 ms, Memory: 21888 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,58 +1,82 @@
 import java.util.*;
 import java.io.*;
 
-
 public class Main {
-    static int N;
-    static boolean[ ] visited;
-    static int[][] graph;
+    static int N,M;
+    static ArrayList<ArrayList<Integer>> map;
+    static boolean[] visited;
+    
     public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        // 정점의 수
-        N = Integer.parseInt(st.nextToken());
-        // 간선의 수
-        int V = Integer.parseInt(st.nextToken());
-        // 시작점
-        int start = Integer.parseInt(st.nextToken());
-        graph = new int[N+1][N+1];
-        for(int i=0;i<V;i++){
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            graph[a][b] = graph[b][a] = 1;
-        }
-            
-        visited = new boolean[N+1];
-        dfs(start);
-        System.out.println();
-        visited = new boolean[N+1];
-        bfs(start);        
+    
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int[] data = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+    
+    N = data[0];
+    M = data[1];
+    int start = data[2];
+    
+    map = new ArrayList<>();
+    
+    for(int i=0;i<N+1;i++){
+        map.add(new ArrayList<Integer>());
     }
-    static void dfs(int Node){
-        visited[Node] = true;
-        System.out.print(Node+" ");
-        for(int i=1;i<N+1;i++){
-            if(graph[Node][i]==1 && !visited[i]){
-                dfs(i);
+    
+    for(int i=0;i<M;i++){
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int index = Integer.parseInt(st.nextToken());
+        int value = Integer.parseInt(st.nextToken());
+        
+        map.get(index).add(value);
+        map.get(value).add(index);
+    }
+    
+    for(ArrayList<Integer> list : map){
+        Collections.sort(list);
+    }
+    
+    visited = new boolean[N+1];
+
+    dfs(start);
+    System.out.println();
+    
+    visited = new boolean[N+1];
+    
+    bfs(start);
+    }
+    
+    static void dfs(int startNode){
+        visited[startNode] = true;
+        System.out.print(startNode+" ");
+        ArrayList<Integer> mapInfo = map.get(startNode);
+        
+        for(int node:  mapInfo){
+            if(!visited[node]){
+                dfs(node);
             }
         }
     }
-    static void bfs(int Node){
-        //  큐에 넣기
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(Node);
-        visited[Node] = true;
-        while(!queue.isEmpty()){
-            int now = queue.poll();
-            System.out.print(now+" ");
-            for(int i=1;i<N+1;i++){
-                if(graph[now][i]==1 && !visited[i]){
-                    queue.add(i);
-                    visited[i] = true;
+    
+    static void bfs(int startNode){
+        visited[startNode] = true;
+        System.out.print(startNode+" ");
+        Queue<Integer> deque = new LinkedList<>();
+        
+        ArrayList<Integer> mapInfo = map.get(startNode);
+        
+        for(int node:mapInfo){
+            deque.add(node);
+        }
+        
+        while(!deque.isEmpty()){
+            int node = deque.poll();
+            if(!visited[node]){
+                visited[node] = true;
+                System.out.print(node+" ");
+                ArrayList<Integer> newInfo = map.get(node);
+                for(int newData:newInfo){
+                    deque.add(newData);
                 }
             }
         }
-        
     }
 }
